369d79dea91855894fe79476d2ff4a767f276c1c
Srivatsan <vatsan1993@gmail.com>
2024-10-15 17:43:34 +0530

    finished project

--- sql-practice/bonus-improve-sql-queries.sql
+++ sql-practice/bonus-improve-sql-queries.sql
@@ -1,89 +1,117 @@
 ----------
--- Step 0 - Create a Query 
+-- Step 0 - Create a Query
 ----------
 -- Query: Find a count of `toys` records that have a price greater than
-    -- 55 and belong to a cat that has the color "Olive".
-
-    -- Your code here
-
+-- 55 and belong to a cat that has the color "Olive".
+-- Your code here
+select count(*)
+from toys
+    join cat_toys on toys.id = cat_toys.toy_id
+    join cats on cats.id = cat_toys.cat_id
+where
+    cats.color = 'Olive'
+    and price > 55;
 -- Paste your results below (as a comment):
-
-
-
+-- 215
 
 ----------
 -- Step 1 - Analyze the Query
 ----------
 -- Query:
+EXPLAIN QUERY PLAN
+select count(*)
+from toys
+    join cat_toys on toys.id = cat_toys.toy_id
+    join cats on cats.id = cat_toys.cat_id
+where
+    cats.color = 'Olive'
+    and price > 55;
 
-    -- Your code here
-
+-- Your code here
 -- Paste your results below (as a comment):
-
-
+-- QUERY PLAN
+--SCAN cat_toys
+--SEARCH toys USING INTEGER PRIMARY KEY (rowid=?)
+--SEARCH cats USING INTEGER PRIMARY KEY (rowid=?)
 -- What do your results mean?
-
-    -- Was this a SEARCH or SCAN?
-
-
-    -- What does that mean?
-
-
-
-
+-- its still a bit inefficient on cat_toys
+-- Was this a SEARCH or SCAN?
+-- scan on cat_toys.
+-- What does that mean?
+-- we need to add an index for both cat_id and toy_id combined.
 ----------
 -- Step 2 - Time the Query to get a baseline
 ----------
 -- Query (to be used in the sqlite CLI):
+.timer ON
+select count(*)
+from toys
+    join cat_toys on toys.id = cat_toys.toy_id
+    join cats on cats.id = cat_toys.cat_id
+where
+    cats.color = 'Olive'
+    and price > 55;
 
-    -- Your code here
-
+.timer off
+-- Your code here
 -- Paste your results below (as a comment):
-
-
-
-
+-- Run Time: real 0.000 user 0.000072 sys 0.000016
 ----------
 -- Step 3 - Add an index and analyze how the query is executing
 ----------
-
 -- Create index:
 
-    -- Your code here
 
+CREATE INDEX idx_toys_price ON toys (price);
+
+CREATE INDEX idx_cats_color ON cats (color);
+
+create index idx_cat_toys_cat_id on cat_toys (cat_id);
+-- Your code here
 -- Analyze Query:
-    -- Your code here
+EXPLAIN QUERY PLAN
+select count(*)
+from toys
+    join cat_toys on toys.id = cat_toys.toy_id
+    join cats on cats.id = cat_toys.cat_id
+where
+    cats.color = 'Olive'
+    and price > 55;
 
+-- Your code here
 -- Paste your results below (as a comment):
-
-
 -- Analyze Results:
-
-    -- Is the new index being applied in this query?
-
-
-
-
+-- QUERY PLAN
+-- |--SEARCH cats USING COVERING INDEX idx_cats_color (color=?)
+-- |--SEARCH cat_toys USING INDEX idx_cat_toys_cat_id (cat_id=?)
+-- `--SEARCH toys USING INTEGER PRIMARY KEY (rowid=?)
+-- Is the new index being applied in this query?
+-- yes
 ----------
 -- Step 4 - Re-time the query using the new index
 ----------
 -- Query (to be used in the sqlite CLI):
+-- Your code here
+.timer ON
+select count(*)
+from toys
+    join cat_toys on toys.id = cat_toys.toy_id
+    join cats on cats.id = cat_toys.cat_id
+where
+    cats.color = 'Olive'
+    and price > 55;
 
-    -- Your code here
-
+.timer off
 -- Paste your results below (as a comment):
-
-
 -- Analyze Results:
-    -- Are you still getting the correct query results?
 
+-- 215
+-- Run Time: real 0.002 user 0.000636 sys 0.001151
 
-    -- Did the execution time improve (decrease)?
-
-
-    -- Do you see any other opportunities for making this query more efficient?
-
-
+-- Are you still getting the correct query results?
+-- Did the execution time improve (decrease)?
+-- improved
+-- Do you see any other opportunities for making this query more efficient?
 
 ---------------------------------
 -- Notes From Further Exploration
--- sql-practice/improve-sql-queries.sql
+++ sql-practice/improve-sql-queries.sql
@@ -1,88 +1,111 @@
 ----------
--- Step 0 - Create a Query 
+-- Step 0 - Create a Query
 ----------
 -- Query: Select all cats that have a toy with an id of 5
 
-    -- Your code here
+-- Your code here
+.headers ON 
+
+select cats.*
+from cats
+    join cat_toys on cats.id = cat_toys.cat_id
+where
+    toy_id = 5;
 
 -- Paste your results below (as a comment):
-
-
-
-
+-- id|name|color|breed
+-- 4002|Rachele|Maroon|Foldex Cat
+-- 31|Rodger|Lavender|Oregon Rex
+-- 77|Jamal|Orange|Sam Sawet
 ----------
 -- Step 1 - Analyze the Query
 ----------
 -- Query:
-
-    -- Your code here
-
+EXPLAIN QUERY PLAN
+select cats.*
+from cats
+    join cat_toys on cats.id = cat_toys.cat_id
+where
+    toy_id = 5;
+-- Your code here
 -- Paste your results below (as a comment):
-
-
+-- QUERY PLAN
+-- SCAN cat_toys
+-- SEARCH cats USING INTEGER PRIMARY KEY (rowid=?)
 -- What do your results mean?
-
-    -- Was this a SEARCH or SCAN?
-
-
-    -- What does that mean?
-
-
-
-
+-- key is not set on the cat_id or toy_id in the cat_toys table
+-- Was this a SEARCH or SCAN?
+-- it was search for cats but scan for cat_toys table
+-- What does that mean?
+-- cat_toys table needs to have index
 ----------
 -- Step 2 - Time the Query to get a baseline
 ----------
 -- Query (to be used in the sqlite CLI):
+-- Your code here
+.timer ON 
 
-    -- Your code here
+select cats.*
+from cats
+    join cat_toys on cats.id = cat_toys.cat_id
+where
+    toy_id = 5;
 
+.timer off
 -- Paste your results below (as a comment):
-
-
-
-
+-- id|name|color|breed
+-- 4002|Rachele|Maroon|Foldex Cat
+-- 31|Rodger|Lavender|Oregon Rex
+-- 77|Jamal|Orange|Sam Sawet
+-- Run Time: real 0.001 user 0.000032 sys 0.000095
 ----------
 -- Step 3 - Add an index and analyze how the query is executing
 ----------
-
 -- Create index:
+-- Your code here
+create index idx_cat_toys_toy_id on cat_toys(toy_id);
 
-    -- Your code here
-
+EXPLAIN QUERY PLAN
+select cats.*
+from cats
+    join cat_toys on cats.id = cat_toys.cat_id
+where
+    toy_id = 5;
 -- Analyze Query:
-    -- Your code here
-
+-- Your code here
 -- Paste your results below (as a comment):
-
-
+-- QUERY PLAN
+--SEARCH cat_toys USING INDEX idx_cat_toys_toy_id (toy_id=?)
+--SEARCH cats USING INTEGER PRIMARY KEY (rowid=?)
 -- Analyze Results:
-
-    -- Is the new index being applied in this query?
-
-
-
-
+-- both are now search
+-- Is the new index being applied in this query?
+-- this will improve the search capability
 ----------
 -- Step 4 - Re-time the query using the new index
 ----------
 -- Query (to be used in the sqlite CLI):
+-- Your code here
+.timer ON 
 
-    -- Your code here
+select cats.*
+from cats
+    join cat_toys on cats.id = cat_toys.cat_id
+where
+    toy_id = 5;
 
+.timer off
 -- Paste your results below (as a comment):
-
-
+-- id|name|color|breed
+-- 4002|Rachele|Maroon|Foldex Cat
+-- 31|Rodger|Lavender|Oregon Rex
+-- 77|Jamal|Orange|Sam Sawet
+-- Run Time: real 0.000 user 0.000027 sys 0.000080
 -- Analyze Results:
-    -- Are you still getting the correct query results?
-
-
-    -- Did the execution time improve (decrease)?
-
-
-    -- Do you see any other opportunities for making this query more efficient?
-
-
+-- the time has reduced
+-- Are you still getting the correct query results?
+-- Did the execution time improve (decrease)?
+-- Do you see any other opportunities for making this query more efficient?
 ---------------------------------
 -- Notes From Further Exploration
 ---------------------------------
